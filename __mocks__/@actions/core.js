b919c191fa2c0551124345ff4340b61088b72b09
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-10-18 12:08:12 +0200

    Clean branch contents

--- __mocks__/@actions/core.js
+++
@@ -1,9 +0,0 @@
-const core = jest.requireActual('@actions/core');
-
-module.exports = {
-  ...core,
-  setSecret: jest.fn(),
-  exportVariable: jest.fn(),
-  setOutput: jest.fn(),
-  setFailed: jest.fn(),
-};
